9b50019e218e31a3133357942b5184053551222c
Anupam Datta <adbd04@gmail.com>
2026-04-26 21:52:26 +0530

    bootrom: replace inline asm jump with function call via configurable address

---
+++ src/bootrom_config.c
@@ -0,0 +1,4 @@
+/* BootROM configuration: address of the payload to jump to.
+   This symbol is defined here and referenced by boot_main.c.
+   Adjust if the payload load address changes. */
+const unsigned long payload_addr = 0x80020000UL;
